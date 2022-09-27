4c7ccda2a75e0cba92d4e3741f0faf4adb094bd9
DavidMacBook <davidmoralesluis@gmail.com>
2022-09-27 12:56:58 +0200

    no copies...laura

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,25 @@
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String cadea="o tempo está xélido";
+        File newText=new File(System.getProperty("user.dir")+"/src/main/java/text5.txt");
+
+        DataOutputStream escribir=new DataOutputStream(new FileOutputStream(newText)); //if true añadir
+        DataInputStream leer=new DataInputStream(new FileInputStream(newText));
+
+        for (int i = 0; i < 2; i++) {
+            escribir.writeChars(cadea);
+
+            System.out.printf("escribiendo :");
+            for (int j = 0; j < cadea.length(); j++) {
+                System.out.print(leer.readChar());
+            }
+            System.out.println();
+        }
+
+        escribir.close();
+        leer.close();
+    }
 }
